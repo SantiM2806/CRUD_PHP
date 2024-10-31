6eafaf421dba713072c3b0fed0eeacdb8779a66b
Santiago <smch2806@gmail.com>
2024-10-30 20:56:45 -0500

    1_MVC_Mostrar Productos

---
+++ Configuracion/conexion.php
@@ -0,0 +1,22 @@
+<?php
+
+//Asignación de variables
+$dsn = 'mysql:host=localhost;port=3306;dbname=hulk';
+$username = 'root';
+$password = '';
+
+try {
+    $conexion = new PDO($dsn, $username, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo 'Conexión exitosa';
+
+} catch (PDOException $e) {
+    echo 'Error de conexión a la BD',$e->getMessage();
+    die();
+ 
+}
+
+
+
+?>
